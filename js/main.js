8f9015fb9a9639f6eb8023729ba2c59543f5229e
ahfadaly <fadaly.dev@gmail.com>
2024-09-18 20:00:44 +0300

    story

---
+++ js/main.js
@@ -0,0 +1,41 @@
+"use strict";
+
+// add class active -----------
+let navlinks = document.querySelectorAll(".header .list li a");
+
+navlinks.forEach((item) => {
+  item.addEventListener(`click`, () => {
+    document.querySelector(".header .list li a.active").classList.remove("active");
+    item.classList.add("active");
+  });
+});
+
+// carousel ---------
+
+$(`document`).ready(() => {
+  //  customer
+  $(".customer-opinions .owl-carousel").owlCarousel({
+    loop: true,
+    margin: 15,
+    rtl: true,
+    autoplay: true,
+    autoplayTimeout: 2000,
+    dots: true,
+    nav: true,
+    navText: [
+      '<button class="slide-arrow prev-arrow"><i class="fa-solid fa-chevron-left"></i></button>',
+      '<button class="slide-arrow prev-arrow"><i class="fa-solid fa-chevron-right"></i></button>',
+    ],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      769: {
+        items: 2,
+      },
+      1200: {
+        items: 3,
+      },
+    },
+  });
+});
